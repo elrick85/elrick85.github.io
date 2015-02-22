5460d33f6830009873ca2d7c5695811af13a38d4
ZIsmailov <zismailov@esphere.ru>
2015-02-22 17:12:56 +0300

    Add markup and fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,57 @@
-module.exports = function(grunt) {
-  'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js'
-      ],
-      options: {
-        "curly": true,
-        "eqeqeq": true,
-        "immed": true,
-        "latedef": true,
-        "newcap": true,
-        "noarg": true,
-        "sub": true,
-        "undef": true,
-        "boss": true,
-        "eqnull": true,
-        "node": true
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js'
+            ],
+            options: {
+                "curly": true,
+                "eqeqeq": true,
+                "immed": true,
+                "latedef": true,
+                "newcap": true,
+                "noarg": true,
+                "sub": true,
+                "undef": true,
+                "boss": true,
+                "eqnull": true,
+                "node": true
+            }
+        },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      test: ['tmp']
-    },
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            test: ['tmp']
+        },
 
-    // Configuration to be run (and then tested).
-    copy: {
-      main: {
-        files: [
-          {expand: true, cwd: 'node_modules/bootstrap/dist/css', src: ['*.css', '!*.min.css'], dest: 'css/'},
-          {expand: true, cwd: 'node_modules/bootstrap/dist/fonts', src: ['**'], dest: 'fonts/'},
-          {expand: true, cwd: 'node_modules/bootstrap/dist/js', src: ['*.js', '!*.min.js'], dest: 'js/'}
-        ]
-      }
-    }
-  });
+        // Configuration to be run (and then tested).
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'node_modules/bootstrap/dist/css', src: ['*.css', '!*.min.css'], dest: 'css/'},
+                    {expand: true, cwd: 'node_modules/bootstrap/dist/fonts', src: ['**'], dest: 'fonts/'},
+                    {expand: true, cwd: 'node_modules/bootstrap/dist/js', src: ['*.js', '!*.min.js'], dest: 'js/'},
+                    {expand: true, cwd: 'node_modules/font-awesome/css', src: ['*.css', '!*.min.css'], dest: 'css/'},
+                    {expand: true, cwd: 'node_modules/font-awesome/fonts', src: ['**'], dest: 'fonts/'}
+
+                ]
+            }
+        }
+    });
 
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('build', ['jshint', 'clean', 'copy']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'copy']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('build', ['jshint', 'clean', 'copy']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'copy']);
 };
